03afc3321b3f5f9034fc02e70cee4664fc30c728
wataruhigasi <ayumu_h130424@outlook.jp>
2023-09-06 23:49:48 +0900

    フォーム

--- kairanban/app/keijiban/articles/page.js
+++ kairanban/app/keijiban/articles/page.js
@@ -1,13 +1,56 @@
+"use client";
+import exp from "constants";
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
-// function Articles() {
-//   return <div>Articles</div>;
-// }
+export default function Articles() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-// export default Articles;
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
 
-const ArticlesPage = () => {
-  return <div>aaaaa</div>;
-};
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-export default ArticlesPage;
+    const Endpoint = "http://localhost:8000/api/articles";
+
+    const formData = {
+      title: title,
+      context: content,
+    };
+
+    try {
+      const response = await axios.post(Endpoint, formData);
+      setTitle("");
+      setContent("");
+    } catch (error) {
+      console.log(formData);
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>記事フォーム</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>タイトル</label>
+          <input type="text" value={title} onChange={handleTitleChange} />
+        </div>
+        <div>
+          <label>内容</label>
+          <textarea value={content} onChange={handleContentChange} />
+        </div>
+        <div>
+          <button type="submit">投稿</button>
+        </div>
+      </form>
+    </div>
+  );
+}

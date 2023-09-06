802dd28983c9a424acaa489921c37a422e27e301
Keyl0ve <kyo.cat34@gmail.com>
2023-09-07 00:15:03 +0900

    modify: button

--- kairanban/app/keijiban/page.js
+++ kairanban/app/keijiban/page.js
@@ -1,14 +1,18 @@
-import Link from "next/link";
+// import Link from "next/link";
 
 const buttonStyle = {
-  backgroundColor: "while",
-  posion: "absolute",
+  backgroundColor: "white", // 背景色をwhiteに修正
+  position: "absolute", // タイポ修正
+  top: "50%", // ボタンを垂直方向の中央に配置
+  left: "50%", // ボタンを水平方向の中央に配置
+  transform: "translate(-50%, -50%)" // ボタンを中央に寄せる
 };
+
 const Page = () => {
   return (
-    <body style={{ backgroundColor: "green" }}>
-      {/* <button style={buttonStyle}>スレッド作成</button> */}
-    </body>
+    <div style={{ backgroundColor: "green", height: "100vh", position: "relative" }}>
+      <button style={buttonStyle}>スレッド作成</button>
+    </div>
   );
 };
 

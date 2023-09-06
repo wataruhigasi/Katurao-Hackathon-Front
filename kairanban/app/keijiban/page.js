785aea69ba41e1c1ce1baa4d412ef324eeb4ad69
wataruhigasi <ayumu_h130424@outlook.jp>
2023-09-07 00:08:08 +0900

    背景

--- kairanban/app/keijiban/page.js
+++ kairanban/app/keijiban/page.js
@@ -1,29 +1,14 @@
-// import React from "react";
-// import { useRouter } from "next/router";
-
-// function Keijiban() {
-//   const router = useRouter();
-//   const linkToarticle = () => {
-//     router.push("/article");
-//   };
-//   return <button onClick={linkToarticle}>Articles</button>;
-// }
-
-// export default Keijiban;
-
 import Link from "next/link";
 
+const buttonStyle = {
+  backgroundColor: "while",
+  posion: "absolute",
+};
 const Page = () => {
   return (
-    <div>
-      <h1>掲示板ページ</h1>
-      <button className="bg-yellow-900 text-white font-bold py-4 px-6 rounded">
-        <Link href="/keijiban/articles">
-          aaaa
-          {/* <a>記事一覧へ移動</a> */}
-        </Link>
-      </button>
-    </div>
+    <body style={{ backgroundColor: "green" }}>
+      {/* <button style={buttonStyle}>スレッド作成</button> */}
+    </body>
   );
 };
 

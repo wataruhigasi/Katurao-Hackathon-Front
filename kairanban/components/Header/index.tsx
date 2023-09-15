198cfda3468801f86b51f17acea3d5f05d8fc08e
matumoto1234 <matumotosizuoka@gmail.com>
2023-09-16 06:28:40 +0900

    fix center point

--- kairanban/components/Header/index.tsx
+++ kairanban/components/Header/index.tsx
@@ -165,8 +165,8 @@ const Header = () => {
                     // @ts-ignore
                     const svg = canvasRef.current.toSVG();
                     postArticle(svg, {
-                      x: canvasRef.current.getCenterPoint().x,
-                      y: canvasRef.current.getCenterPoint().y,
+                      x: 10000 / 2 + 300,
+                      y: 10000 / 2 + 300,
                     });
                   }
                 }}

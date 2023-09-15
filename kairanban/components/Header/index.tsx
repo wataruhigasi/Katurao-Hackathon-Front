269d1641919bd4b5628cd0c6adf4a7f7c17d9877
wataruhigasi <ayumu_h130424@outlook.jp>
2023-09-15 13:00:36 +0900

    スレッド

--- kairanban/components/Header/index.tsx
+++ kairanban/components/Header/index.tsx
@@ -32,20 +32,48 @@ const postArticle = (
     });
 };
 
+const postThread = (
+  svg: string,
+  position: {
+    x: number;
+    y: number;
+  }
+): void => {
+  fetch("http://localhost:8080/thread", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: svg,
+      position,
+    }),
+  })
+    .then((resp) => {
+      if (!resp.ok) {
+        console.log("スレッドの作成に失敗しました: ", resp);
+      }
+    })
+    .catch((err) => {
+      console.error("スレッドの作成に失敗しました: ", err);
+    });
+};
+
 const CANVAS_ID = "create-canvas";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [canvasSize, setCanvasSize] = useState({
+    width: 480 - 16 * 2,
+    height: 480 * Math.sqrt(2) - 16 * 2,
+    isDrawingMode: true,
+  });
 
   // 完了ボタンを押すときにcanvasを保持しておきたい
   const canvasRef = React.useRef<fabric.Canvas | null>(null);
 
   useEffect(() => {
-    const newCanvas = new fabric.Canvas(CANVAS_ID, {
-      width: 480 - 16 * 2,
-      height: 480 * Math.sqrt(2) - 16 * 2,
-      isDrawingMode: true,
-    });
+    const newCanvas = new fabric.Canvas(CANVAS_ID, canvasSize);
     newCanvas.freeDrawingBrush = new fabric.PencilBrush(newCanvas);
 
     canvasRef.current = newCanvas;
@@ -60,9 +88,53 @@ const Header = () => {
   return (
     <header className={styles.headerStyle}>
       <div className={styles.buttonContainer}>
-        <button className={styles.button}>
-          <Link href="/keijiban/threads/new">スレッド作成</Link>
-        </button>
+        <Popup
+          trigger={<button className={styles.button}>スレッド作成</button>}
+          modal
+          overlayStyle={{ background: "rgba(0,0,0,0.5)" }}
+          closeOnDocumentClick={false}
+          onOpen={() => {
+            setCanvasSize({ width: 200, height: 200, isDrawingMode: true });
+            setIsOpen(true);
+          }}
+          onClose={() => {
+            setIsOpen(false);
+            setCanvasSize({
+              width: 480 - 16 * 2,
+              height: 480 * Math.sqrt(2) - 16 * 2,
+              isDrawingMode: true,
+            });
+          }}
+        >
+          {/* This is a reactjs-popup style. See also https://react-popup.elazizi.com/react-modal */}
+          {/* @ts-ignore */}
+          {(close) => (
+            <>
+              <div className={styles.popupThreadContent}>
+                <canvas id={CANVAS_ID} className={styles.createArticleCanvas} />
+              </div>
+              <button
+                onClick={() => {
+                  close();
+                  if (canvasRef.current) {
+                    // toSVG()の引数は無いとTSに怒られるが、なぜか動くっぽい(公式が引数なしで書いてる)
+                    // @ts-ignore
+                    const svg = canvasRef.current.toSVG();
+                    postThread(svg, {
+                      x: canvasRef.current.getCenterPoint().x,
+                      y: canvasRef.current.getCenterPoint().y,
+                    });
+                  }
+                }}
+              >
+                完了
+              </button>
+              <div className={styles.close} onClick={close}>
+                &times;
+              </div>
+            </>
+          )}
+        </Popup>
 
         <Popup
           trigger={<button className={styles.button}>記事作成</button>}

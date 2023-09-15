3ff9b8f82cbd277b3922e3215aea5fbe0bf8d674
wataruhigasi <ayumu_h130424@outlook.jp>
2023-09-15 10:52:05 +0900

    スレッド書き込み完了

--- kairanban/components/Header/index.tsx
+++ kairanban/components/Header/index.tsx
@@ -9,22 +9,52 @@ const CANVAS_ID = "create-canvas";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [canvasSize, setCanvasSize] = useState({
+    width: 480 - 16 * 2,
+    height: 480 * Math.sqrt(2) - 16 * 2,
+    isDrawingMode: true,
+  });
 
   useEffect(() => {
-    const canvas = new fabric.Canvas(CANVAS_ID, {
-      width: 480 - 16 * 2,
-      height: 480 * Math.sqrt(2) - 16 * 2,
-      isDrawingMode: true,
-    });
+    const canvas = new fabric.Canvas(CANVAS_ID, canvasSize);
     canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
   }, [isOpen]);
 
   return (
     <header className={styles.headerStyle}>
       <div className={styles.buttonContainer}>
-        <button className={styles.button}>
-          <Link href="/keijiban/threads/new">スレッド作成</Link>
-        </button>
+        <Popup
+          trigger={<button className={styles.button}>スレッド作成</button>}
+          modal
+          overlayStyle={{ background: "rgba(0,0,0,0.5)" }}
+          closeOnDocumentClick={false}
+          onOpen={() => {
+            setCanvasSize({ width: 200, height: 200, isDrawingMode: true });
+            setIsOpen(true);
+          }}
+          onClose={() => {
+            setCanvasSize({
+              width: 480 - 16 * 2,
+              height: 480 * Math.sqrt(2) - 16 * 2,
+              isDrawingMode: true,
+            });
+            setIsOpen(false);
+          }}
+        >
+          {/* This is a reactjs-popup style. See also https://react-popup.elazizi.com/react-modal */}
+          {/* @ts-ignore */}
+          {(close) => (
+            <>
+              <div className={styles.popupThreadContent}>
+                <canvas id={CANVAS_ID} className={styles.createArticleCanvas} />
+              </div>
+              <button onClick={close}>完了</button>
+              <div className={styles.close} onClick={close}>
+                &times;
+              </div>
+            </>
+          )}
+        </Popup>
 
         <Popup
           trigger={<button className={styles.button}>記事作成</button>}
@@ -42,7 +72,7 @@ const Header = () => {
           {/* @ts-ignore */}
           {(close) => (
             <>
-              <div className={styles.popupContent}>
+              <div className={styles.popupArticleContent}>
                 <canvas id={CANVAS_ID} className={styles.createArticleCanvas} />
               </div>
               <button onClick={close}>完了</button>

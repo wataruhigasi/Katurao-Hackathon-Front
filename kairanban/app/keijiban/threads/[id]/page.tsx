06aad93bd71e682489433104fa2a554da9878385
matumoto1234 <matumotosizuoka@gmail.com>
2023-09-15 18:29:53 +0900

    threadの落書きを実装

--- kairanban/app/keijiban/threads/[id]/page.tsx
+++ kairanban/app/keijiban/threads/[id]/page.tsx
@@ -5,10 +5,65 @@ import styles from "./threads.module.css";
 import ModeButton from "../../../../components/ModeButton";
 import ReturnIcon from "../../../../public/ReturnIcon.svg";
 import * as fabric from "fabric";
+import { useParams } from "next/navigation";
+import router from "next/router";
 
 const CANVAS_ID = "threads-canvas";
 
+type RakugakisResponse = {
+  id: number;
+  created_at: string;
+  body: string;
+  position: {
+    x: number;
+    y: number;
+  };
+}[];
+
+const getRakugakis = async (threadId: string): Promise<RakugakisResponse> => {
+  const resp = await fetch(
+    `http://localhost:8080/thread/${threadId}/rakugakis`
+  );
+  if (!resp.ok) {
+    console.error("らくがきの取得に失敗しました: ", resp);
+    return;
+  }
+  return await resp.json();
+};
+
+const postPath = (
+  path: fabric.Path,
+  position: {
+    x: number;
+    y: number;
+  },
+  threadId: string
+): void => {
+  fetch(`http://localhost:8080/thread/${threadId}/rakugaki`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // toSVG()の引数がなくてTSに怒られるけど、公式がそう書いてる
+      // @ts-ignore
+      body: path.toSVG(),
+      position,
+    }),
+  })
+    .then((resp) => {
+      if (!resp.ok) {
+        console.error("らくがきの保存に失敗しました: ", resp);
+      }
+    })
+    .catch((err) => {
+      console.error("らくがきの送信に失敗しました: ", err);
+    });
+};
+
 const ThreadsPage: React.FC = () => {
+  const params = useParams();
+
   useEffect(() => {
     window.scrollTo(10 / 2, 10 / 2);
   }, []);
@@ -23,6 +78,7 @@ const ThreadsPage: React.FC = () => {
   const isEdit = mode === "edit";
 
   const canvasRef = useRef<fabric.Canvas | null>(null);
+
   useEffect(() => {
     const newCanvas = new fabric.Canvas(CANVAS_ID, {
       height: 1000,
@@ -31,7 +87,28 @@ const ThreadsPage: React.FC = () => {
     });
     newCanvas.freeDrawingBrush = new fabric.PencilBrush(newCanvas);
 
-    canvasRef.current = newCanvas;
+    newCanvas.on("mouse:up", (e: any) => {
+      if (e.currentTarget.aCoords) {
+        postPath(
+          e.currentTarget,
+          e.currentTarget.aCoords.tl,
+          params.id as string
+        );
+      }
+    });
+
+    getRakugakis(params.id as string).then((rakugakis) => {
+      rakugakis.forEach((rakugaki) => {
+        fabric.loadSVGFromString(rakugaki.body).then((svg) => {
+          if (svg.objects[0]) {
+            newCanvas.add(svg.objects[0]);
+          }
+        });
+      });
+
+      canvasRef.current = newCanvas;
+    });
+
     return () => {
       if (canvasRef.current) {
         canvasRef.current.dispose();
@@ -45,7 +122,7 @@ const ThreadsPage: React.FC = () => {
       <div className={styles.keijibanBackground}>
         <canvas
           id={CANVAS_ID}
-          className={mode === "edit" ? styles.zIndexPlus : styles.zIndexMinus}
+          className={mode === "edit" ? styles.zIndexPlus : styles.zIndexZero}
         />
       </div>
       <div className={styles.buttonContainer}>
